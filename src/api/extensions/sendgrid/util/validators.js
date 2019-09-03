be480c3c8e94c7fd7b461bfdbf01042c37bb62fc
Fifciuu <filip.jdrasik@gmail.com>
2019-09-03 12:48:26 +0200

    Date validator update

--- src/api/extensions/sendgrid/util/validators.js
+++ src/api/extensions/sendgrid/util/validators.js
@@ -2,7 +2,7 @@ module.exports = {
   email: (v) => /\S+@\S+\.\S+/.test(v),
   phone: (v) => /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/m.test(v) || /^[1-9][0-9]{8}$/.test(v),
   name: (v) => /^(([A-Za-zĄąęĘóćĆłŁńŚśźŹżŻ]+[\-\']?)*([A-Za-zĄąęĘóćĆłŁńŚśźŹżŻ]+)?\s)+([A-Za-zĄąęĘóćĆłŁńŚśźŹżŻ]+[\-\']?)*([A-Za-zĄąęĘóćĆłŁńŚśźŹżŻ]+)?$/.test(v),
-  orderDate: (v) => /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/.test(v),
+  orderDate: (v) => new Date(v) <= new Date(),
   reason: (v) => v.length >= 2,
   accountNumber: (v) => v.length >= 14,
   products: (v) => {

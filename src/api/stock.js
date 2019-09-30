75ae5f67682b9765ba993a99b1937e9a1ad8fbb3
Fifciuu <filip.jdrasik@gmail.com>
2019-09-30 21:28:38 +0200

    Remove debugger

--- src/api/stock.js
+++ src/api/stock.js
@@ -24,7 +24,6 @@ export default ({ config, db }) => {
 
 		stockProxy.check(req.params.sku).then((result) => {
 
-			console.log(result)
 			apiStatus(res, result, 200);
 		}).catch(err=> {
 			apiStatus(res, err, 500);
@@ -72,3 +71,71 @@ export default ({ config, db }) => {
 
 	return stockApi
 }
+
+/**
+ * stockStatuses
+ * {
+    "product_id": 2078,
+    "stock_id": 1,
+    "qty": 0,
+    "stock_status": 0,
+    "stock_item": {
+        "item_id": 2600,
+        "product_id": 2078,
+        "stock_id": 1,
+        "qty": 2,
+        "is_in_stock": true,
+        "is_qty_decimal": false,
+        "show_default_notification_message": false,
+        "use_config_min_qty": true,
+        "min_qty": 0,
+        "use_config_min_sale_qty": 1,
+        "min_sale_qty": 1,
+        "use_config_max_sale_qty": true,
+        "max_sale_qty": 10000,
+        "use_config_backorders": true,
+        "backorders": 0,
+        "use_config_notify_stock_qty": true,
+        "notify_stock_qty": 1,
+        "use_config_qty_increments": true,
+        "qty_increments": 0,
+        "use_config_enable_qty_inc": true,
+        "enable_qty_increments": false,
+        "use_config_manage_stock": true,
+        "manage_stock": true,
+        "low_stock_date": null,
+        "is_decimal_divided": false,
+        "stock_status_changed_auto": 0
+    }
+}
+
+* stockItems
+{
+    "item_id": 2600,
+    "product_id": 2078,
+    "stock_id": 1,
+    "qty": 2,
+    "is_in_stock": true,
+    "is_qty_decimal": false,
+    "show_default_notification_message": false,
+    "use_config_min_qty": true,
+    "min_qty": 0,
+    "use_config_min_sale_qty": 1,
+    "min_sale_qty": 1,
+    "use_config_max_sale_qty": true,
+    "max_sale_qty": 10000,
+    "use_config_backorders": true,
+    "backorders": 0,
+    "use_config_notify_stock_qty": true,
+    "notify_stock_qty": 1,
+    "use_config_qty_increments": true,
+    "qty_increments": 0,
+    "use_config_enable_qty_inc": true,
+    "enable_qty_increments": false,
+    "use_config_manage_stock": true,
+    "manage_stock": true,
+    "low_stock_date": null,
+    "is_decimal_divided": false,
+    "stock_status_changed_auto": 0
+}
+ */
